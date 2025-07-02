b8e59a1da25d9572b71e79b7b582f7faf5c68e4a
Vadim Cherepenichev <vadim.cvy@gmail.com>
2025-07-02 21:50:19 +0200

    PDF Generator - Browser can't start in  github workflow. Trying to fix it

--- scripts/generatePDF.ts
+++ scripts/generatePDF.ts
@@ -24,7 +24,9 @@ export const generatePDF = async () =>
 
   try
   {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
 
     const page = await browser.newPage()
 
